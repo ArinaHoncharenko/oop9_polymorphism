010a75c4d4f8bd471d84d87872a680f0f0e6885e
ArinaHoncharenko <118198844+ArinaHoncharenko@users.noreply.github.com>
2023-11-23 20:12:55 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static oop9_products.Form1;
 
 namespace oop9_products
 {
@@ -16,36 +17,47 @@ namespace oop9_products
         {
             public string Name { get; set; }
             public double Price { get; set; }
-            public double Weight { get; set; }
-            public string Brand { get; set; }
-            public bool InStock { get; set; }
+            public int Quantity { get; set; }
 
             public virtual double CalculateTotal()
             {
-                return Price * Weight;
+                return Price * Quantity;
             }
         }
 
-        public class Meat : Product
+        public class Clothing : Product
         {
-            public Meat(string name, double price, double weight, string brand, bool inStock)
+            public string Size { get; set; }
+            public Clothing(string name, double price, int quantity, string size)
             {
                 Name = name;
                 Price = price;
-                Weight = weight;
-                Brand = brand;
-                InStock = inStock;
+                Quantity = quantity;
+                Size = size;
             }
         }
 
-        public class Vegetables : Product
+        public class Book : Product
         {
-            public Vegetables(string name, double price, double weight, bool inStock)
+            public string Author { get; set; }
+            public Book(string name, double price, int quantity, string author)
             {
                 Name = name;
                 Price = price;
-                Weight = weight;
-                InStock = inStock;
+                Quantity = quantity;
+                Author = author;
+            }
+        }
+
+        public class Bag : Product
+        {
+            public string Brend { get; set; }
+            public Bag(string name, double price, int quantity, string brend)
+            {
+                Name = name;
+                Price = price;
+                Quantity = quantity;
+                Brend = brend;
             }
         }
 
@@ -74,30 +86,76 @@ namespace oop9_products
             }
         }
 
+        private List<Product> storeProducts = new List<Product>();
+
         public Form1()
         {
             InitializeComponent();
+
+            storeProducts.Add(new Clothing("Футболка", 350, 1, "M"));
+            storeProducts.Add(new Clothing("Шорти", 400.7, 1, "S"));
+            storeProducts.Add(new Clothing("Светер", 670, 1, "XL"));
+            storeProducts.Add(new Book("Кобзар", 380.5, 1, "Т. Г. Шевченко"));
+            storeProducts.Add(new Book("Тіні забутих предків", 180, 1, "М. Коцюбинський"));
+            storeProducts.Add(new Book("Тигролови", 200, 1, "І. Багряний"));
+            storeProducts.Add(new Bag("Рюкзак", 512, 1, "Baggg"));
+
+            // Обновите список продуктов в магазине
+            UpdateStoreProductList();
+
+            foreach (Product product in storeProducts)
+            {
+                comboBoxProductName.Items.Add(product.Name);
+            }
+
+            // Встановлюємо обраний продукт за замовчуванням (перший продукт у списку)
+            comboBoxProductName.SelectedIndex = 0;
         }
+        private void UpdateStoreProductList()
+        {
+            listBoxStoreProducts.Items.Clear();
+            foreach (Product product in storeProducts)
+            {
+                listBoxStoreProducts.Items.Add($"{product.Name} - {product.CalculateTotal()} UAH");
+            }
+        }
+
 
         private ShoppingCart cart = new ShoppingCart();
 
         private void buttonAddToCart_Click(object sender, EventArgs e)
         {
-            // Код для добавления товара в корзину
-            string productName = textBoxProductName.Text;
-            double price = 10.99; // Ваша логика получения цены
-            double weight = Convert.ToDouble(numericUpDownQuantity.Value); // Извлечение веса из числового поля
-            string brand = "SomeBrand"; // Ваша логика получения бренда
-            bool inStock = true; // Ваша логика определения наличия
+            // Отримуємо обране значення з comboBoxProductName
+            string selectedProductName = comboBoxProductName.SelectedItem.ToString();
 
-            Meat meatProduct = new Meat(productName, price, weight, brand, inStock);
-            cart.AddToCart(meatProduct);
+            // Знаходимо продукт в магазині за його іменем
+            Product selectedProduct = storeProducts.Find(product => product.Name == selectedProductName);
 
-            // Очистка полей после добавления товара
-            textBoxProductName.Clear();
-            numericUpDownQuantity.Value = 0;
+            // Код для добавлення обраного продукту в корзину
+            double price = selectedProduct.Price;
+            int quantity = Convert.ToInt32(numericUpDownQuantity.Value);
 
-            // Обновление списка продуктов в корзине
+            // Вибір категорії продукту
+            if (selectedProduct is Book)
+            {
+                Book selectedBook = selectedProduct as Book;
+                cart.AddToCart(new Book(selectedBook.Name, price, quantity, selectedBook.Author));
+            }
+            else if (selectedProduct is Clothing)
+            {
+                Clothing selectedClothing = selectedProduct as Clothing;
+                cart.AddToCart(new Clothing(selectedClothing.Name, price, quantity, selectedClothing.Size));
+            }
+            else if (selectedProduct is Bag)
+            {
+                Bag selectedBag = selectedProduct as Bag;
+                cart.AddToCart(new Bag(selectedBag.Name, price, quantity, selectedBag.Brend));
+            }
+
+            // Очищення полів після добавлення товару
+            numericUpDownQuantity.Value = 1;
+
+            // Оновлення списку продуктів в корзині
             UpdateShoppingCartList();
         }
 
@@ -105,7 +163,7 @@ namespace oop9_products
         {
             // Код для расчета и вывода общей стоимости
             double total = cart.CalculateTotal();
-            labelTotal.Text = $"Total: {total:C}";
+            labelTotal.Text = $"Сума: {total} UAH";
         }
 
         // Метод для обновления списка продуктов в ListBox
@@ -114,8 +172,9 @@ namespace oop9_products
             listBoxShoppingCart.Items.Clear();
             foreach (Product product in cart.Products)
             {
-                listBoxShoppingCart.Items.Add($"{product.Name} - {product.CalculateTotal():C}");
+                listBoxShoppingCart.Items.Add($"{product.Name} - {product.CalculateTotal()} UAH");
             }
         }
+
     }
 }
