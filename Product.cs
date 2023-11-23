76413afa3ed308a953ca942fc52e9c7995acac0d
ArinaHoncharenko <118198844+ArinaHoncharenko@users.noreply.github.com>
2023-11-23 23:42:28 +0200

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace oop9_products
+{
+    public abstract class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public virtual double CalculateTotal()
+        {
+            return Price * Quantity;
+        }
+    }
+
+    public class Clothing : Product
+    {
+        public string Size { get; set; }
+
+        public Clothing(string name, double price, int quantity, string size)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+            Size = size;
+        }
+    }
+
+    public class Book : Product
+    {
+        public string Author { get; set; }
+
+        public Book(string name, double price, int quantity, string author)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+            Author = author;
+        }
+    }
+
+    public class Bag : Product
+    {
+        public string Brend { get; set; }
+
+        public Bag(string name, double price, int quantity, string brend)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+            Brend = brend;
+        }
+    }
+
+    public class Fruit : Product
+    {
+        public double Weight { get; set; }
+
+        public Fruit(string name, double price, int quantity, double weight)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+            Weight = weight;
+        }
+    }
+}
---
+++ ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace oop9_products
+{
+    public class ShoppingCart
+    {
+        private List<Product> products = new List<Product>();
+
+        public List<Product> Products
+        {
+            get { return products; }
+        }
+
+        public void AddToCart(Product product)
+        {
+            products.Add(product);
+        }
+
+        public double CalculateTotal()
+        {
+            double total = 0;
+            foreach (Product product in products)
+            {
+                total += product.CalculateTotal();
+            }
+            return total;
+        }
+    }
+}
