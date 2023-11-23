c89d2e3c41aed6cc4692f74a2329fa08af3bb736
ArinaHoncharenko <118198844+ArinaHoncharenko@users.noreply.github.com>
2023-11-23 18:57:19 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace oop9_products
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxProductName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownQuantity = new System.Windows.Forms.NumericUpDown();
+            this.buttonAddToCart = new System.Windows.Forms.Button();
+            this.listBoxShoppingCart = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonCalculateTotal = new System.Windows.Forms.Button();
+            this.labelTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownQuantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxProductName
+            // 
+            this.textBoxProductName.Location = new System.Drawing.Point(93, 12);
+            this.textBoxProductName.Name = "textBoxProductName";
+            this.textBoxProductName.Size = new System.Drawing.Size(120, 20);
+            this.textBoxProductName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Product Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Quantity";
+            // 
+            // numericUpDownQuantity
+            // 
+            this.numericUpDownQuantity.Location = new System.Drawing.Point(93, 39);
+            this.numericUpDownQuantity.Name = "numericUpDownQuantity";
+            this.numericUpDownQuantity.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownQuantity.TabIndex = 2;
+            // 
+            // buttonAddToCart
+            // 
+            this.buttonAddToCart.Location = new System.Drawing.Point(15, 65);
+            this.buttonAddToCart.Name = "buttonAddToCart";
+            this.buttonAddToCart.Size = new System.Drawing.Size(198, 23);
+            this.buttonAddToCart.TabIndex = 4;
+            this.buttonAddToCart.Text = "Add to Cart";
+            this.buttonAddToCart.UseVisualStyleBackColor = true;
+            this.buttonAddToCart.Click += new System.EventHandler(this.buttonAddToCart_Click);
+            // 
+            // listBoxShoppingCart
+            // 
+            this.listBoxShoppingCart.FormattingEnabled = true;
+            this.listBoxShoppingCart.Location = new System.Drawing.Point(15, 114);
+            this.listBoxShoppingCart.Name = "listBoxShoppingCart";
+            this.listBoxShoppingCart.Size = new System.Drawing.Size(198, 95);
+            this.listBoxShoppingCart.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Shopping Cart:";
+            // 
+            // buttonCalculateTotal
+            // 
+            this.buttonCalculateTotal.Location = new System.Drawing.Point(15, 215);
+            this.buttonCalculateTotal.Name = "buttonCalculateTotal";
+            this.buttonCalculateTotal.Size = new System.Drawing.Size(198, 23);
+            this.buttonCalculateTotal.TabIndex = 7;
+            this.buttonCalculateTotal.Text = "Calculate Total";
+            this.buttonCalculateTotal.UseVisualStyleBackColor = true;
+            this.buttonCalculateTotal.Click += new System.EventHandler(this.buttonCalculateTotal_Click);
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.Location = new System.Drawing.Point(12, 241);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(0, 13);
+            this.labelTotal.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelTotal);
+            this.Controls.Add(this.buttonCalculateTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBoxShoppingCart);
+            this.Controls.Add(this.buttonAddToCart);
+            this.Controls.Add(this.numericUpDownQuantity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxProductName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownQuantity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxProductName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownQuantity;
+        private System.Windows.Forms.Button buttonAddToCart;
+        private System.Windows.Forms.ListBox listBoxShoppingCart;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonCalculateTotal;
+        private System.Windows.Forms.Label labelTotal;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oop9_products
+{
+    public partial class Form1 : Form
+    {
+        public class Product
+        {
+            public string Name { get; set; }
+            public double Price { get; set; }
+            public double Weight { get; set; }
+            public string Brand { get; set; }
+            public bool InStock { get; set; }
+
+            public virtual double CalculateTotal()
+            {
+                return Price * Weight;
+            }
+        }
+
+        public class Meat : Product
+        {
+            public Meat(string name, double price, double weight, string brand, bool inStock)
+            {
+                Name = name;
+                Price = price;
+                Weight = weight;
+                Brand = brand;
+                InStock = inStock;
+            }
+        }
+
+        public class Vegetables : Product
+        {
+            public Vegetables(string name, double price, double weight, bool inStock)
+            {
+                Name = name;
+                Price = price;
+                Weight = weight;
+                InStock = inStock;
+            }
+        }
+
+        public class ShoppingCart
+        {
+            private List<Product> products = new List<Product>();
+
+            public List<Product> Products
+            {
+                get { return products; }
+            }
+
+            public void AddToCart(Product product)
+            {
+                products.Add(product);
+            }
+
+            public double CalculateTotal()
+            {
+                double total = 0;
+                foreach (Product product in products)
+                {
+                    total += product.CalculateTotal();
+                }
+                return total;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private ShoppingCart cart = new ShoppingCart();
+
+        private void buttonAddToCart_Click(object sender, EventArgs e)
+        {
+            // Код для добавления товара в корзину
+            string productName = textBoxProductName.Text;
+            double price = 10.99; // Ваша логика получения цены
+            double weight = Convert.ToDouble(numericUpDownQuantity.Value); // Извлечение веса из числового поля
+            string brand = "SomeBrand"; // Ваша логика получения бренда
+            bool inStock = true; // Ваша логика определения наличия
+
+            Meat meatProduct = new Meat(productName, price, weight, brand, inStock);
+            cart.AddToCart(meatProduct);
+
+            // Очистка полей после добавления товара
+            textBoxProductName.Clear();
+            numericUpDownQuantity.Value = 0;
+
+            // Обновление списка продуктов в корзине
+            UpdateShoppingCartList();
+        }
+
+        private void buttonCalculateTotal_Click(object sender, EventArgs e)
+        {
+            // Код для расчета и вывода общей стоимости
+            double total = cart.CalculateTotal();
+            labelTotal.Text = $"Total: {total:C}";
+        }
+
+        // Метод для обновления списка продуктов в ListBox
+        private void UpdateShoppingCartList()
+        {
+            listBoxShoppingCart.Items.Clear();
+            foreach (Product product in cart.Products)
+            {
+                listBoxShoppingCart.Items.Add($"{product.Name} - {product.CalculateTotal():C}");
+            }
+        }
+    }
+}
